1fb0e64853e706f47207d9f5ef9945777b1820b4
barkbuff <connect.arindamhore@gmail.com>
2025-12-04 11:25:42 +0530

    Push Alignment Clarity

--- frontend/workbench/src/pages/Auth/Login.jsx
+++ frontend/workbench/src/pages/Auth/Login.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Login = () => {
   return (
     <> 
-       <div className="text-5xl text-orange-600">SandBox</div>
-      <div className="text-5xl text-orange-600">Sand</div>
+      <div>
+        SandBox
+      </div>
     </>
     
   )
