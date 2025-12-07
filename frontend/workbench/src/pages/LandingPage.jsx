cbd35b8dadf77a57aa0dcc2f79cda70238225600
Harsh Vardhan <harshvardhan012006@gmail.com>
2025-12-07 22:33:07 +0530

    Push the new changes in landingpage and made new ProfileInfoCard.jsx file in the generic codebase

--- frontend/workbench/src/pages/LandingPage.jsx
+++ frontend/workbench/src/pages/LandingPage.jsx
@@ -6,8 +6,12 @@ import { LuSparkles } from "react-icons/lu";
 import Modal from "../components/Modal";
 import Login from "./Auth/Login";
 import SignUp from "./Auth/SignUp";
+import { UserContext } from "../context/userContext";
+import { useContext } from "react";
+import ProfileInfoCard from "../Cards/ProfileInfoCard";
 
 const LandingPage = () => {
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
   const [openAuthModal, setOpenAuthModal] = useState(false);
   const [currentPage, setCurrentPage] = useState("login");
@@ -25,12 +29,15 @@ const LandingPage = () => {
           <header className="flex justify-between items-center mb-16">
             <div className="text-xl text-black font-bold">MindCraft</div>
 
-            <button
+            {user ? (
+              <ProfileInfoCard/>
+             ) : (
+             <button
               className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
               onClick={() => setOpenAuthModal(true)}
             >
               Login / SignUp
-            </button>
+            </button>)}
           </header>
 
           {/* Hero Content */}
